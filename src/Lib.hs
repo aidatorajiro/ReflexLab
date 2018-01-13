f6b6beeae087692f733265c848882e9ec7a10232
aidatorajiro <kawarusosu@zoho.com>
2018-01-13 16:14:47 +0900

    upgraded reflex version.

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,3 +1,6 @@
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE OverloadedStrings #-}
+
 module Lib
     ( startApp ) where
 
@@ -6,51 +9,31 @@ import LibReflex hiding (makeStyle)
 import qualified Nested as N
 
 import qualified Data.Map as M
-import qualified Data.Tree as T
+import qualified Data.Text as T
+import Data.Tree (unfoldTree)
+import Data.Text.Encoding (decodeUtf8)
 import Reflex.Dom
 import Data.Maybe (isNothing, fromJust)
 import Safe (atMay)
 import System.Random (getStdRandom, random)
+import Data.FileEmbed (embedFile)
 
 makeStyle :: MonadWidget t m => m ()
-makeStyle = el "style" $ text "\
-\body {\
-\    color: #333;\
-\}\
-\#selectModule, #selectModuleTogglable, #treeModule {\
-\    margin: 1em;\
-\    padding: 1em;\
-\    border: 1px #324 solid;\
-\    border-radius: 4px;\
-\    display: inline-block;\
-\    vertical-align: top;\
-\}\
-\#selectModule {\
-\    border: 1px #324 solid;\
-\}\
-\#treeModule {\
-\    border: 1px #9817b2 solid;\
-\}"
+makeStyle = el "style" $ text $ decodeUtf8 $(embedFile "assets/css/Lib.css")
 
 selectModule :: MonadWidget t m => m ()
 selectModule = elId "div" "selectModule" $ do
     elAttr "h1" (M.fromList [("style", "margin-top: 0;")]) $ text "select module #1"
 
-    item <- selectList_value ["aaaaa", "bbbbb", "aiueo", "kakikukeko"]
+    item <- selectListValue ["aaaaa", "bbbbb", "aiueo", "kakikukeko"]
 
-    txt1 <- forDyn item $ \i -> do
-        show i
-
-    item_change <- foldDyn (:) [] (updated item)
-
-    txt2 <- forDyn item_change $ \x ->
-        if atMay x 0 == atMay x 1 then "" else "=== VALUE CHANGED ==="
-
-    dynText txt1
+    dynText $ T.pack <$> show <$> item
 
     br
 
-    dynText txt2
+    txt <- fmap (\(x:y:_) -> if x == y then "" else "=== VALUE CHANGED ===") <$> foldDyn (:) [Nothing, Nothing] (updated item)
+
+    dynText txt
 
     return ()
 
@@ -58,12 +41,9 @@ selectModuleTogglable :: MonadWidget t m => m ()
 selectModuleTogglable = elId "div" "selectModuleTogglable" $ do
     elAttr "h1" (M.fromList [("style", "margin-top: 0;")]) $ text "select module #2"
 
-    item <- selectListTogglable_value ["aaaaa", "bbbbb", "aiueo", "kakikukeko"]
+    item <- selectListTogglableValue ["aaaaa", "bbbbb", "aiueo", "kakikukeko"]
 
-    txt <- forDyn item $ \i -> do
-        show i
-
-    dynText txt
+    dynText $ T.pack <$> show <$> item
 
     return ()
 
@@ -74,7 +54,7 @@ treeModule seed = elId "div" "treeModule" $ do
 
     let init_ext = N.Universe seed (N.E init_ext)
 
-    drawTree $ T.unfoldTree (\(N.E x) -> (N.name x, N.childs x)) $ N.E init_ext
+    drawTree $ unfoldTree (\(N.E x) -> (T.pack $ N.name x, N.childs x)) $ N.E init_ext
 
     return ()
 
--- src/LibReflex.hs
+++ src/LibReflex.hs
@@ -1,67 +1,39 @@
+{-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE RecursiveDo #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TupleSections #-}
 
 module LibReflex where
 
+import qualified Data.Text as T
 import qualified Data.Map as M
-import qualified Data.Tree as T
+import Data.Text.Encoding (decodeUtf8)
+import Data.Tree
 import LibTree (treeAt)
 import Reflex.Dom
 import Data.Maybe (isNothing, fromJust)
 import Safe (atMay)
+import Data.FileEmbed (embedFile)
 
 makeStyle :: MonadWidget t m => m ()
-makeStyle = el "style" $ text "\
-\.selectList {\
-\    user-select: none;\
-\    cursor: default;\
-\    font-size: 12px;\
-\}\
-\.selectList .item {\
-\    padding: 0px;\
-\    margin: 12px;\
-\}\
-\.button {\
-\    width: 10px;\
-\    height:10px;\
-\    background: white;\
-\    border: 1px gray solid;\
-\    display: inline-block;\
-\    margin-right: 0.5em;\
-\}\
-\.selectList .button.enabled {\
-\    background: black;\
-\}\
-\.selectList .item:hover .button, .tree .label:hover .button {\
-\    padding-right: 4px;\
-\}\
-\.tree .button.enabled {\
-\    background: black;\
-\}\
-\.tree .childs {\
-\    padding-left: 1em;\
-\}\
-\.tree .childs.none {\
-\    display: none;\
-\}"
+makeStyle = el "style" $ text $ decodeUtf8 $(embedFile "assets/css/LibReflex.css")
 
-elId :: MonadWidget t m => String -> String -> m a -> m a
+elId :: MonadWidget t m => T.Text -> T.Text -> m a -> m a
 elId tagName id func = elAttr tagName (M.fromList [("id", id)]) func
 
-elClass' :: MonadWidget t m => String -> String -> m a -> m (El t, a)
-elClass' tagName className func = elAttr' tagName (M.fromList [("class", className)]) func
+-- elClass' :: MonadWidget t m => T.Text -> T.Text -> m a -> m (El t, a)
+-- elClass' tagName className func = elAttr' tagName (M.fromList [("class", className)]) func
 
-div_attrsDyn :: MonadWidget t m => Dynamic t (M.Map String String) -> m (El t, Event t ())
+div_attrsDyn :: MonadWidget t m => Dynamic t (M.Map T.Text T.Text) -> m (El t, Event t ())
 div_attrsDyn attrsDyn = do
-    (elm, _) <- elDynAttr' "div" attrsDyn $ text ""
+    (elm, _) <- elDynAttr' "div" attrsDyn (return ())
     return (elm, domEvent Click elm)
 
-generate_button_style :: Bool -> M.Map String String
+generate_button_style :: Bool -> M.Map T.Text T.Text
 generate_button_style bool = M.fromList [("class", (if bool == True then "button enabled" else "button"))]
 
 generate_button :: MonadWidget t m => Dynamic t Bool -> m ( El t, Event t () )
-generate_button boolDyn = do
-    attrsDyn <- forDyn boolDyn $ \b -> generate_button_style b
-    div_attrsDyn attrsDyn
+generate_button = div_attrsDyn . fmap (\b -> generate_button_style b)
 
 btn :: MonadWidget t m => m (Dynamic t Bool)
 btn = mdo
@@ -69,47 +41,51 @@ btn = mdo
     (btn_elem, events) <- generate_button bool
     return bool
 
-generateSelectList :: MonadWidget t m => ([Event t Int] -> m (Dynamic t Int)) -> [String] -> m ( Dynamic t Int )
+-- | generate selective list from a function that transforms user's click event to a Dynamic Int value. returns index.
+generateSelectList :: MonadWidget t m => ([Event t Int] -> m (Dynamic t Int)) -> [T.Text] -> m ( Dynamic t Int )
 generateSelectList events_arr_func arr =
     return =<< elClass "div" "selectList" $ mdo
         index <- events_arr_func events_arr
         events_arr <- mapM (\x -> do
             (elm, _) <- elClass' "div" "item" $ do
-                generate_button =<< (forDyn index $ \i -> x == i)
+                generate_button $ fmap (==x) index
                 text $ arr !! x
             return $ tag (constant x) $ domEvent Click elm) $ take (length arr) [0..]
         return index
 
-generateSelectList_value :: MonadWidget t m => ([Event t Int] -> m (Dynamic t Int)) -> [String] -> m ( Dynamic t (Maybe String) )
-generateSelectList_value events_arr_func arr = do
-    index <- generateSelectList events_arr_func arr
-    value <- forDyn index $ \i -> atMay arr i
-    return value
+-- | generate selective list from a function that transforms user's click event to a Dynamic Int value. returns value.
+generateSelectListValue :: MonadWidget t m => ([Event t Int] -> m (Dynamic t Int)) -> [T.Text] -> m ( Dynamic t (Maybe T.Text) )
+generateSelectListValue events_arr_func arr = fmap (atMay arr) <$> generateSelectList events_arr_func arr
 
-selectList :: MonadWidget t m => [String] -> m ( Dynamic t Int )
-selectList       = generateSelectList       (\x -> foldDyn (\e p -> e) (-1) $ leftmost x)
+-- | generate selective list from a list of text. returns index.
+selectList :: MonadWidget t m => [T.Text] -> m ( Dynamic t Int )
+selectList      = generateSelectList       (\x -> foldDyn (\e p -> e) (-1) $ leftmost x)
 
-selectList_value :: MonadWidget t m => [String] -> m ( Dynamic t (Maybe String) )
-selectList_value = generateSelectList_value (\x -> foldDyn (\e p -> e) (-1) $ leftmost x)
+-- | generate selective list from a list of text. returns value.
+selectListValue :: MonadWidget t m => [T.Text] -> m ( Dynamic t (Maybe T.Text) )
+selectListValue = generateSelectListValue (\x -> foldDyn (\e p -> e) (-1) $ leftmost x)
 
-selectListTogglable :: MonadWidget t m => [String] -> m ( Dynamic t Int )
-selectListTogglable       = generateSelectList       (\x -> foldDyn (\e p -> if e == p then -1 else e) (-1) $ leftmost x)
+-- | generate selective list from a list of text. returns index.
+selectListTogglable :: MonadWidget t m => [T.Text] -> m ( Dynamic t Int )
+selectListTogglable      = generateSelectList       (\x -> foldDyn (\e p -> if e == p then -1 else e) (-1) $ leftmost x)
 
-selectListTogglable_value :: MonadWidget t m => [String] -> m ( Dynamic t (Maybe String) )
-selectListTogglable_value = generateSelectList_value (\x -> foldDyn (\e p -> if e == p then -1 else e) (-1) $ leftmost x)
+-- | generate selective list from a list of text. returns value.
+selectListTogglableValue :: MonadWidget t m => [T.Text] -> m ( Dynamic t (Maybe T.Text) )
+selectListTogglableValue = generateSelectListValue (\x -> foldDyn (\e p -> if e == p then -1 else e) (-1) $ leftmost x)
 
-drawTree :: MonadWidget t m => T.Tree String -> m ()
+-- | draw a tree
+drawTree :: MonadWidget t m => Tree T.Text -> m ()
 drawTree tree = elClass "div" "tree" $ drawTreeMain tree []
 
 -- # of clicks                     : 0 1 2 3 4 5 6 7 8 9 ...
 -- whether the button is enabled   : F T F T F T F T F T ...
 -- whether child items exists      : F T T T T T T T T T ...
 -- whether child items are visible : F T F T F T F T F T ...
-drawTreeMain :: MonadWidget t m => T.Tree String -> [Int] -> m ()
+drawTreeMain :: MonadWidget t m => Tree T.Text -> [Int] -> m ()
 drawTreeMain tree pos = mdo
     let this        = treeAt tree pos
-        this_label  = T.rootLabel this
-        this_forest = T.subForest this
+        this_label  = rootLabel this
+        this_forest = subForest this
 
     (elm, _) <- elClass' "div" "label" $ do
         generate_button bool_button
@@ -118,15 +94,15 @@ drawTreeMain tree pos = mdo
     bool_button <- toggle False $ domEvent Click elm                 -- whether the button is enabled / whether child items are visible
     bool_childs <- foldDyn (\x y -> True) False $ domEvent Click elm -- whether child items exists
 
-    let child_empty_list = M.fromList $ map (\k -> (k, ())) $ take (length this_forest) [0..]
-    childs <- forDyn bool_childs $ \b -> if b then child_empty_list else M.fromList []
+    let child_empty_list = M.fromList $ map (,()) $ take (length this_forest) [0..]
+        childs  = fmap (\b ->                       if b then child_empty_list else M.empty      ) bool_childs
+        attrDyn = fmap (\b -> M.singleton "class" $ if b then "childs"         else "childs none") bool_button
 
-    attrDyn <- forDyn bool_button $ \b -> M.fromList [if b then ("class", "childs") else ("class", "childs none")]
     elDynAttr "div" attrDyn $ do
         listWithKey childs $ \k v ->
             let child        = treeAt tree $ pos ++ [k]
-                child_label  = T.rootLabel child
-                child_forest = T.subForest child
+                child_label  = rootLabel child
+                child_forest = subForest child
              in if child_forest == [] then elClass "div" "label" $ text child_label else drawTreeMain tree $ pos ++ [k]
 
     return ()
